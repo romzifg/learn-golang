1ab6af06b6141efa3fae44972b34eba183e73633
Romzi Farhan Ghozi <romzifg@gmail.com>
2022-10-27 01:41:06 +0700

    update

---
+++ golang_web/golang_template_test.go
@@ -0,0 +1,84 @@
+package golang_web
+
+import (
+	"embed"
+	_ "embed"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"text/template"
+)
+
+func SimpleHTML(writer http.ResponseWriter, request *http.Request) {
+	templateText := `<html><body>{{.}}</body></html>`
+	// t, err := template.New("SIMPLE").Parse(templateText)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	t := template.Must(template.New("SIMPLE").Parse(templateText))
+
+	t.ExecuteTemplate(writer, "SIMPLE", "Hello HTML Template")
+}
+
+func SimpleHMLTFile(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.ParseFiles("./templates/simple.gohtml"))
+	t.ExecuteTemplate(writer, "simple.gohtml", "Hello HTML Template")
+}
+
+func TemplateDirectory(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.ParseGlob("./templates/*.gohtml"))
+
+	t.ExecuteTemplate(writer, "simple.gohtml", "Hello HTML Template")
+}
+
+//go:embed templates/*.gohtml
+var templates embed.FS
+
+func TemplateFileEmbed(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.ParseFS(templates, "templates/*.gohtml"))
+
+	t.ExecuteTemplate(writer, "simple.gohtml", "Hello HTML Template")
+}
+
+func TestSimpleHTML(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	SimpleHTML(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TestSimpleHTMLFile(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	SimpleHMLTFile(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TestTemplateDirectory(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateDirectory(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TestTemplateEmbed(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateFileEmbed(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
---
+++ golang_web/template_data_test.go
@@ -0,0 +1,65 @@
+package golang_web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"text/template"
+)
+
+func TemplateDataMap(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.ParseFiles("./templates/name.gohtml"))
+	t.ExecuteTemplate(writer, "name.gohtml", map[string]interface{}{
+		"Title": "Template Data Map",
+		"Name":  "Romzi Farhan Ghozi",
+		"Address": map[string]interface{}{
+			"Street": "Perum Abdinegara",
+			"City":   "Bandung",
+		},
+	})
+}
+
+type Address struct {
+	Street string
+	City   string
+}
+
+type Page struct {
+	Title   string
+	Name    string
+	Address Address
+}
+
+func TemplateDataStruct(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.ParseFiles("./templates/name.gohtml"))
+	t.ExecuteTemplate(writer, "name.gohtml", Page{
+		Title: "Template Data Struct",
+		Name:  "Romzi",
+		Address: Address{
+			Street: "Perum Abdinegara",
+			City:   "Bandung",
+		},
+	})
+}
+
+func TestTemplateDataMap(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateDataMap(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TestTemplateDataStruct(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateDataStruct(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
