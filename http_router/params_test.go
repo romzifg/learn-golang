26453db05cb42bbffadf36ab32ae820ce3107dc6
Romzi Farhan Ghozi <romzifg@gmail.com>
2022-12-08 21:41:57 +0700

    update golang router

---
+++ http_router/params_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRouterParams(t *testing.T) {
+	router := httprouter.New()
+	router.GET("/products/:id", func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+		text := "Product " + params.ByName("id")
+		fmt.Fprint(writer, text)
+	})
+
+	request := httptest.NewRequest("GET", "http://localhost:3000/products/1", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, "Product 1", string(body))
+}
