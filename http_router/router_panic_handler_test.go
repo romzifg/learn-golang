fbc19293d43664888cc9868c6d3bfba4ee747b0e
Romzi Farhan Ghozi <romzifg@gmail.com>
2022-12-08 22:02:05 +0700

    update

---
+++ http_router/router_panic_handler_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPanic(t *testing.T) {
+	router := httprouter.New()
+	router.PanicHandler = func(w http.ResponseWriter, r *http.Request, error interface{}) {
+		fmt.Fprint(w, "Panic : ", error)
+	}
+
+	router.GET("/", func(writer http.ResponseWriter, request *http.Request, _ httprouter.Params) {
+		panic("Upss")
+	})
+
+	request := httptest.NewRequest("GET", "http://localhost:3000/", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, "Panic : Upss", string(body))
+}
